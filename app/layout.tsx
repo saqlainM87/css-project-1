dd846870b755310d93897b678d9003d470d3d90d
saqlainM87 <saqlain.mahmud@monstar-lab.com>
2023-09-12 10:36:23 +0600

    refactor: add content component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Header from '@/components/Header/Header';
+import { Content } from 'antd/es/layout/layout';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,7 +19,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 <Header />
-                {children}
+                <Content>{children}</Content>
             </body>
         </html>
     );
