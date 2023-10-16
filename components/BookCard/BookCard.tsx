6bf1800b1afc04ebf11d983230fffa90158a7a2d
saqlainM87 <saqlain.mahmud@monstar-lab.com>
2023-10-16 11:37:46 +0600

    feat: add book card

--- components/BookCard/BookCard.tsx
+++ components/BookCard/BookCard.tsx
@@ -1,23 +1,50 @@
 'use client';
 
-import { Card } from 'antd';
+import { Button, Card, Col, Row } from 'antd';
+import cx from 'classnames';
+import Image from 'next/image';
+
+import DownloadWhiteIcon from '@/assets/icons/DownloadWhite.svg';
+import heartOutlinedIcon from '@/assets/icons/ant-design_heart-outlined.svg';
+
+import harlemShuffleImage from '@/assets/images/9781982185824 2.png';
+
+import styles from './bookCard.module.scss';
 
 const { Meta } = Card;
 
 const BookCard = () => {
     return (
-        <Card
-            className="w-[17.125rem]"
-            hoverable
-            cover={
-                <img
-                    alt="example"
-                    src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-                />
-            }
-        >
-            <Meta title="Europe Street beat" description="www.instagram.com" />
-        </Card>
+        <div>
+            <Card
+                className={cx('w-[17.125rem]', styles.bookCard)}
+                hoverable
+                cover={<Image alt="example" src={harlemShuffleImage} />}
+            >
+                <Meta title="Harlem Shuffle" description="Colson Whitehead" />
+                <Row className="mt-4" justify="space-between">
+                    <Col className="text-xl font-semibold">$26.92</Col>
+                    <Col>
+                        <Button
+                            type="text"
+                            icon={<Image src={heartOutlinedIcon} alt="like" />}
+                        />
+                    </Col>
+                </Row>
+            </Card>
+            <Button
+                type="primary"
+                className="rounded-sm text-2xl font-medium px-8 py-3 w-full h-full text-white border-custom-purple-1 bg-custom-purple-1 flex justify-center items-center"
+                icon={
+                    <Button
+                        type="text"
+                        icon={<Image src={DownloadWhiteIcon} alt="cart" />}
+                    />
+                }
+            >
+                Add to cart
+            </Button>
+        </div>
     );
 };
 
